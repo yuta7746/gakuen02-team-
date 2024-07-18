f3cea2386896166ee54c043153ee6ac7c4a8f0bf
yuta7746 <yuta_shiraki@mail.toyota.co.jp>
2024-07-18 14:02:50 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+// ここに JavaScript のコードを書きましょう
+
+
+
+document.getElementById("card1").addEventListener("click",
+function () {
+    location.replace("https://store.alpen-group.jp");
+},false);
